89591ab8eaa73e7cf8bf06519491921e54e4a142
Ioannis Iordanidis <ioannis@icloud.com>
2025-01-08 21:50:45 +0000

    Renamed the project

--- src/main/java/com/github/michelinman/leetcode/LeetcodeApplication.java
+++ src/main/java/com/github/michelinman/leetcode/LeetcodeApplication.java
@@ -6,10 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import static com.github.michelinman.leetcode.foobar.FooBarRunner.foobar;
 
 @SpringBootApplication
-public class LeetcodeApplication {
+public class LeetCodeApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(LeetcodeApplication.class, args);
+        SpringApplication.run(LeetCodeApplication.class, args);
 
         foobar();
     }
