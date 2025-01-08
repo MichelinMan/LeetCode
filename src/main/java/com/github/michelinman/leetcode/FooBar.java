b5a96bcedfb879c3aa5bfa6d9c7e7b413ef840fe
Ioannis Iordanidis <ioannis@icloud.com>
2025-01-08 21:17:14 +0000

    Added comments to the class

--- src/main/java/com/github/michelinman/leetcode/FooBar.java
+++ src/main/java/com/github/michelinman/leetcode/FooBar.java
@@ -15,34 +15,38 @@ class FooBar {
         this.n = n;
     }
 
+    // Method to print "foo" n times, ensuring it alternates with "bar"
     public void foo(Runnable printFoo) throws InterruptedException {
         for (int i = 0; i < n; i++) {
-            lock.lock();
+            lock.lock(); // Acquire the lock
             try {
+                // Wait until it's foo's turn
                 while (!fooTurn) {
                     fooCondition.await();
                 }
-                printFoo.run();
-                fooTurn = false;
-                barCondition.signal();
+                printFoo.run(); // Print "foo"
+                fooTurn = false; // Set turn to bar
+                barCondition.signal(); // Signal bar thread to proceed
             } finally {
-                lock.unlock();
+                lock.unlock(); // Release the lock
             }
         }
     }
 
+    // Method to print "bar" n times, ensuring it alternates with "foo"
     public void bar(Runnable printBar) throws InterruptedException {
         for (int i = 0; i < n; i++) {
-            lock.lock();
+            lock.lock(); // Acquire the lock
             try {
+                // Wait until it's bar's turn
                 while (fooTurn) {
                     barCondition.await();
                 }
-                printBar.run();
-                fooTurn = true;
-                fooCondition.signal();
+                printBar.run(); // Print "bar"
+                fooTurn = true; // Set turn to foo
+                fooCondition.signal(); // Signal foo thread to proceed
             } finally {
-                lock.unlock();
+                lock.unlock(); // Release the lock
             }
         }
     }
