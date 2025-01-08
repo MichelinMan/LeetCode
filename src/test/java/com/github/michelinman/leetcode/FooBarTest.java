70c4dcd2896f6cf1331f809541dc22c8c2c470f8
Ioannis Iordanidis <ioannis@icloud.com>
2025-01-08 21:15:25 +0000

    Added tests to the class

---
+++ src/test/java/com/github/michelinman/leetcode/FooBarTest.java
@@ -0,0 +1,118 @@
+package com.github.michelinman.leetcode;
+
+import org.junit.jupiter.api.Test;
+import java.util.concurrent.atomic.AtomicInteger;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class FooBarTest {
+
+    @Test
+    void fooBarPrintsFoobarOnce() throws InterruptedException {
+        FooBar fooBar = new FooBar(1);
+        StringBuilder output = new StringBuilder();
+        Thread threadA = new Thread(() -> {
+            try {
+                fooBar.foo(() -> output.append("foo"));
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        });
+        Thread threadB = new Thread(() -> {
+            try {
+                fooBar.bar(() -> output.append("bar"));
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        });
+
+        threadA.start();
+        threadB.start();
+        threadA.join();
+        threadB.join();
+
+        assertEquals("foobar", output.toString());
+    }
+
+    @Test
+    void fooBarPrintsFoobarMultipleTimes() throws InterruptedException {
+        FooBar fooBar = new FooBar(3);
+        StringBuilder output = new StringBuilder();
+        Thread threadA = new Thread(() -> {
+            try {
+                fooBar.foo(() -> output.append("foo"));
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        });
+        Thread threadB = new Thread(() -> {
+            try {
+                fooBar.bar(() -> output.append("bar"));
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        });
+
+        threadA.start();
+        threadB.start();
+        threadA.join();
+        threadB.join();
+
+        assertEquals("foobarfoobarfoobar", output.toString());
+    }
+
+    @Test
+    void fooBarHandlesEdgeCaseWithZero() throws InterruptedException {
+        FooBar fooBar = new FooBar(0);
+        StringBuilder output = new StringBuilder();
+        Thread threadA = new Thread(() -> {
+            try {
+                fooBar.foo(() -> output.append("foo"));
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        });
+        Thread threadB = new Thread(() -> {
+            try {
+                fooBar.bar(() -> output.append("bar"));
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        });
+
+        threadA.start();
+        threadB.start();
+        threadA.join();
+        threadB.join();
+
+        assertEquals("", output.toString());
+    }
+
+    @Test
+    void fooBarHandlesMaximumValue() throws InterruptedException {
+        FooBar fooBar = new FooBar(1000);
+        AtomicInteger fooCount = new AtomicInteger();
+        AtomicInteger barCount = new AtomicInteger();
+        Thread threadA = new Thread(() -> {
+            try {
+                fooBar.foo(fooCount::incrementAndGet);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        });
+        Thread threadB = new Thread(() -> {
+            try {
+                fooBar.bar(barCount::incrementAndGet);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        });
+
+        threadA.start();
+        threadB.start();
+        threadA.join();
+        threadB.join();
+
+        assertEquals(1000, fooCount.get());
+        assertEquals(1000, barCount.get());
+    }
+}
