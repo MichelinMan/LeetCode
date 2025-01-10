95a3b228f094bca4ba4c0cd3fcd78d8fb03fcb9d
Ioannis Iordanidis <ioannis@icloud.com>
2025-01-10 16:25:45 +0000

    printZeroEvenOdd grokked

--- src/main/java/com/github/michelinman/leetcode/LeetcodeApplication.java
+++ src/main/java/com/github/michelinman/leetcode/LeetcodeApplication.java
@@ -18,14 +18,14 @@ public class LeetCodeApplication {
     public static void main(String[] args) throws InterruptedException {
         SpringApplication.run(LeetCodeApplication.class, args);
 
-        runFooBar(10);
+//        runFooBar(10);
         runZeroEvenOdd(5);
-        runFoo(new int[]{1, 3, 2});
-        runH2O("OOHHHH");
-        runFizzBuzz(15);
-        runTrafficLightExamples();
-        runDinningPhilosophers(5);
-        runHtmlParserExamples();
+//        runFoo(new int[]{1, 3, 2});
+//        runH2O("OOHHHH");
+//        runFizzBuzz(15);
+//        runTrafficLightExamples();
+//        runDinningPhilosophers(5);
+//        runHtmlParserExamples();
     }
 
 }
