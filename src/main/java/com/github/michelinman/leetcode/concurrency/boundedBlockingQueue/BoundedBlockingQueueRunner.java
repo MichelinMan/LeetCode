000c7b8b87981db5c220ad084dd1cacad1a3e32c
Ioannis Iordanidis <ioannis@icloud.com>
2025-01-11 16:05:37 +0000

    Removed unused @Slf4j annotation

--- src/main/java/com/github/michelinman/leetcode/concurrency/boundedBlockingQueue/BoundedBlockingQueueRunner.java
+++ src/main/java/com/github/michelinman/leetcode/concurrency/boundedBlockingQueue/BoundedBlockingQueueRunner.java
@@ -1,13 +1,10 @@
 package com.github.michelinman.leetcode.concurrency.boundedBlockingQueue;
 
-import lombok.extern.slf4j.Slf4j;
-
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-@Slf4j
 public class BoundedBlockingQueueRunner {
     public static void runBoundedBlockingQueue(int capacity, int numProducers, int numConsumers, List<String> sequence) throws InterruptedException {
         BoundedBlockingQueue queue = new BoundedBlockingQueue(capacity);
